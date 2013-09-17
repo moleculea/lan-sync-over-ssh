e7aa38cc6540c1c3842b5292fec8f522496587bd
Shichao An <moleculeandrew@gmail.com>
2013-09-17 17:54:18 -0400

    fix utility logic

--- ssh_sync.sh
+++ ssh_sync.sh
@@ -12,15 +12,16 @@ mac_address="11:11:11:11:11:11"
 # -----------------------------
 
 
-usage="Usage: $0 hosts|dns|test <remote_src_path> <local_dest_path>"
+usage="Usage:\n\
+$0 hosts|dns <remote_src_path> <local_dest_path>\n\
+$0 test [<ip_address>]"
 
 if [ "$#" -lt 3 ]; then
 	if [ "$1" != "test" ]; then
-		echo $usage
+		echo -e $usage
 		exit 1
 	fi
 fi
-
 
 # Zone file of BIND server (original file for the symbolic link)
 zone_file="$user_home/Documents/named/$hostname.zone"
@@ -28,16 +29,21 @@ zone_file="$user_home/Documents/named/$hostname.zone"
 # Get updated IP address
 ip_address=$(arp -a | grep $mac_address | cut -d' ' -f2 | tr -d '()')
 if [ -z "$ip_address" ]; then
-	echo "No IP address of host $hostname with MAC $mac_address on the LAN is detected."
-	echo "The host may be down or not accessible at this time."
-	exit 1
+	if [ "$1" != "test" ]; then
+		echo "No IP address of host $hostname with MAC $mac_address on the LAN is detected."
+		echo "The host may be down or not accessible at this time."
+		echo "Please run the following command if you know the IP address of $hostname:"
+		echo "$0 test <ip_address>"
+		exit 1
+	fi
 fi
 sed_cmd="/$hostname/d"
 
 # Update hostname to /etc/hosts
 if [ "$1" == "hosts" ]; then
 	echo "Updating /etc/hosts"
-	sudo bash -c "sed $sed_cmd /etc/hosts > $user_home/tmp/hosts && cat $user_home/tmp/hosts > /etc/hosts"
+	sudo bash -c "sed $sed_cmd /etc/hosts > $user_home/tmp/hosts && cat \
+	$user_home/tmp/hosts > /etc/hosts"
 	sudo bash -c "echo $ip_address $hostname >> /etc/hosts"
 
 # Update zone file
@@ -51,10 +57,19 @@ elif [ "$1" == "dns" ]; then
 	rndc stop
 
 elif [ "$1" == "test" ]; then
-	mdkir $user_home/tmp
-	echo "Hostname $hostname can be mapped to IP address $ip_address ($mac_address)."
+	if [ ! -d "$user_home/tmp" ]; then
+		mkdir $user_home/tmp
+	fi
+	if [ -n "$2" ]; then
+		ip_address="$2"
+	elif [ -z "$ip_address" ]; then
+		echo "No IP address of $hostname is retrieved."
+		echo "Please specifiy IP address for test: $0 test <ip_address>"
+		exit 1
+	fi
 	echo "Pinging $ip_address..."
-	ping -c 4 $ip_address
+	ping -c 4 $ip_address > /dev/null
+	echo "Hostname $hostname can be mapped to IP address $ip_address ($mac_address)."
 	echo "Ready to sync."
 	exit 0
 
